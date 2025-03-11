7cddb8c80ada656098ec49c8a53333c1cec0fadc
Filip Kopecký <kopecfi3@student.cvut.cz>
2025-03-11 07:32:54 +0000

    Delete Main.java

--- src/main/java/cz/cvut/fel/pjv/Main.java
+++
@@ -1,8 +0,0 @@
-package cz.cvut.fel.pjv;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("new feature");
-    }
-}
